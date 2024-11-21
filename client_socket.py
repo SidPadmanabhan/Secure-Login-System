5ceb11cc47f4e987a598b2c4ddb140cd2999a0b4
SidPadmanabhan <88408627+SidPadmanabhan@users.noreply.github.com>
2024-11-21 17:53:29 -0500

    Update client_socket.py

--- client_socket.py
+++ client_socket.py
@@ -1,20 +1,14 @@
-import threading
 import socket
 
 try:
-    cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET --> types of address your program can work with
+    cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("[C]: Client socket created")
 except socket.error as err:
-    print('socket open error: {} \n'.format(err))
+    print(f"Socket open error: {err}")
     exit()
 
-# connect to the server on local machine
 server_binding = ("localhost", 9999)
 cs.connect(server_binding)
-
-data_from_server=cs.recv(1024)
-print("[C]: Data received from server: " + message)    
-cs.send(input("Response here: ").encode())
 
 email = input("Enter your email: ")
 cs.send(email.encode())
@@ -22,8 +16,10 @@ cs.send(email.encode())
 password = input("Enter your password: ")
 cs.send(password.encode())
 
-login_response = cs.recv(1024)
-print("[C]: Login response from server: " + login_response.decode())
+
+login_response = cs.recv(1024).decode()
+print(login_response)
+
 
 cs.close()
 print("[C]: Connection closed")
